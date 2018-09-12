1d90f19042187e4def7416106211167590a4103e
Mike F Robbins <mikefrobbins@users.noreply.github.com>
2018-09-12 15:43:39 -0500

    Updated comment to reflect recent changes to logic

--- Template/Module.psm1
+++ Template/Module.psm1
@@ -1,4 +1,4 @@
-﻿#Dot source all functions in all ps1 files located in the module\public folder
+﻿#Dot source all functions in all ps1 files located in the module's public and private folder
 Get-ChildItem -Path $PSScriptRoot\public\*.ps1, $PSScriptRoot\private\*.ps1 -Exclude *.tests.ps1, *profile.ps1 |
 ForEach-Object {
     . $_.FullName
